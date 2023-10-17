86239cafb1e78a3ac868132a06e8518a0f71d4b9
Pirroxx <service@magicgenetics.com>
2023-10-17 15:13:25 +0200

    new commit

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import FlipCountdown from "@rumess/react-flip-countdown";
 import car from "../assets/Asset 1.svg";
 import phone from "../assets/phonesc.svg";
@@ -8,9 +9,29 @@ import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
 import "@leenguyen/react-flip-clock-countdown/dist/index.css";
 
 const CountdownClock: React.FC = () => {
+  const [os, setOS] = useState("");
+
+  useEffect(() => {
+    const userAgent = window.navigator.userAgent;
+    const platform = window.navigator.platform;
+
+    if (/Win/.test(platform)) {
+      setOS("Windows");
+    } else if (/Linux/.test(platform)) {
+      setOS("Linux");
+    } else {
+      setOS("Unknown OS");
+    }
+  }, []);
+  const isWindows = window.navigator.platform.indexOf("Win") > -1;
   const targetTime = new Date().getTime() + 48 * 7200 * 2000 + 10000;
+  const isMacOS = navigator.userAgent.includes("Macintosh");
   return (
-    <div className="down-cont">
+    <div
+      className={
+        isWindows ? "down-cont" : isMacOS ? "mac-class-container" : "down-cont"
+      }
+    >
       <div className="fliper">
         <FlipClockCountdown
           to={targetTime}
@@ -18,7 +39,18 @@ const CountdownClock: React.FC = () => {
           className="flip"
         />
       </div>
-      <div className="images-cont">
+      <div
+        className={
+          isWindows ? "new-classname" : isMacOS ? "mac-class" : "images-cont"
+        }
+        // className={
+        //   os === "Linux"
+        //     ? "images-cont"
+        //     : os === "Windows"
+        //     ? "new-classname"
+        //     : ""
+        // }
+      >
         <Image loading="lazy" className="car-img" src={car} alt="" />
         <Image className="phone-img" src={phone} alt="" />
       </div>
--- src/components/Ridees.tsx
+++ src/components/Ridees.tsx
@@ -1,14 +1,40 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import "./Riddees.css";
 import Image from "next/image";
 import Ridee from "../assets/Ridees.svg";
 type Props = {};
 
 const Ridees = (props: Props) => {
+  const [os, setOS] = useState("");
+
+  useEffect(() => {
+    const userAgent = window.navigator.userAgent;
+    const platform = window.navigator.platform;
+
+    if (/Win/.test(platform)) {
+      setOS("Windows");
+    } else if (/Linux/.test(platform)) {
+      setOS("Linux");
+    } else {
+      setOS("Unknown OS");
+    }
+  }, []);
+  const isWindows = window.navigator.platform.indexOf("Win") > -1;
+  const isMacOS = navigator.userAgent.includes("Macintosh");
   return (
     <div className="ridees-cont">
       <Image className="img-rid" src={Ridee} alt="" />
-      <h1 className="header-cno">BRINGING EVERY TRIP CLOSER WITH A TAP</h1>
+      <h1
+        className={
+          isWindows ? "new-windos" : isMacOS ? "mac-class-text" : "header-cno"
+        }
+        // className={
+        //   os === "Linux" ? "header-cno" : os === "Windows" ? "new-windos" : ""
+        // }
+      >
+        BRINGING EVERY TRIP CLOSER WITH A TAP
+      </h1>
     </div>
   );
 };
